459bf41066c9e88a2eb55075fc2e960ca83c4d2d
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-04-18 16:26:22 -0300

    build: Cria o projeto

---
+++ src/main/java/com/curso/dougllas/mscloudgateway/MsCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.curso.dougllas.mscloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCloudGatewayApplication.class, args);
+    }
+
+}
