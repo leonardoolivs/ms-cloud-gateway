728728fe11a615e6193f280b056108a5a23301d0
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-04-18 20:59:17 -0300

    build: Adiciona EnableDiscoveryClient e rota de ms-clientes

--- src/main/java/com/curso/dougllas/mscloudgateway/MsCloudGatewayApplication.java
+++ src/main/java/com/curso/dougllas/mscloudgateway/MsCloudGatewayApplication.java
@@ -2,12 +2,25 @@ package com.curso.dougllas.mscloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsCloudGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MsCloudGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder){
+        return builder
+                .routes()
+                .route(r -> r.path("/clientes/**").uri("lb://ms-clientes"))
+                .build();
     }
 
 }
